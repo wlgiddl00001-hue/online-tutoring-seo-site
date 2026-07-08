60cace5844e5e42baff0f21a741723bbf93d354f
Gabin Kim <wlgiddl00001@gmail.com>
2026-07-08 16:20:04 +0900

    Add sitemap and robots

---
+++ make-sitemap.js
@@ -0,0 +1,28 @@
+﻿const fs = require("fs");
+const pages = require("./src/data/pages.json");
+
+const site = "https://online-tutoring-seo-site.vercel.app";
+
+const urls = [site].concat(
+  pages.map(function (page) {
+    return site + "/" + page.slug;
+  })
+);
+
+let xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+
+urls.forEach(function (url) {
+  const priority = url === site ? "1.0" : "0.8";
+  xml += "  <url>";
+  xml += "<loc>" + url + "</loc>";
+  xml += "<changefreq>weekly</changefreq>";
+  xml += "<priority>" + priority + "</priority>";
+  xml += "</url>\n";
+});
+
+xml += "</urlset>\n";
+
+fs.writeFileSync("public/sitemap.xml", xml, "utf8");
+
+console.log("sitemap created " + urls.length);
---
+++ public/make-sitemap.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const pages = require("./src/data/pages.json");
+
+const site = "https://online-tutoring-seo-site.vercel.app";
+
+const urls = [
+  site,
+  ...pages.map((page) => `${site}/${page.slug}`),
+];
+
+const xml =
+  `<?xml version="1.0" encoding="UTF-8"?>\n` +
+  `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n` +
+  urls
+    .map((url) => {
+      const priority = url === site ? "1.0" : "0.8";
+      return `  <url><loc>${url}</loc><changefreq>weekly</changefreq><priority>${priority}</priority></url>`;
+    })
+    .join("\n") +
+  `\n</urlset>\n`;
+
+fs.writeFileSync("public/sitemap.xml", xml, "utf8");
+
+console.log(`sitemap created ${urls.length}`);
